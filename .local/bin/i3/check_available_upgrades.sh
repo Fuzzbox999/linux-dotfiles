1aa9b934f5b8a2b4b3cb82c41d32fb3ae177fcee
Fuzzbox999 <fuzzbox.flc@gmail.com>
2019-08-26 11:04:54 +0200

    Typos

--- .local/bin/i3/check_available_upgrades.sh
+++ .local/bin/i3/check_available_upgrades.sh
@@ -6,9 +6,9 @@
 # In order to work, the pacman packages list needs to be updated.
 # On my machine this is done every 15 minutes by a root crontab
 # */15 * * * * /usr/bin/pacman -Sy >> /dev/null 2>&1
-# This is dangerous because it can lead to partial upgrades of the system if a package is installed afterwards by only using 
-# "pacman -S packagename". That's whay I ALWAYS use pacman -Syu packaganame ton install a new package.
-# I could use 'checkupdates', but it does not update the packages list, and I want my notification to be updated every minute so it disappears# quickly after an effective upgrade of the# system, without querying an Arch mirror 3600 times per hour...
+# This is DANGEROUS because it can lead to partial upgrades of the system if a package is installed afterwards by only using 
+# "pacman -S packagename". That's why I ALWAYS use pacman -Syu packaganame to install a new package.
+# I could use 'checkupdates', but it does not update the packages list, and it annoys me for several reasons.
 
 installed=$(pacman -Q|wc -l)
 #upgrade=$(checkupdates|wc -l)
