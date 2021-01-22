e77d814d881aae2e2e7a8ba9f6ca9f962f4b774d
Fuzzbox999 <fuzzbox.flc@gmail.com>
2021-01-22 07:42:17 +0100

    A patch was missing

--- .dwm/dwm/config.def.h
+++ .dwm/dwm/config.def.h
@@ -15,6 +15,17 @@ static const int user_bh            = 32;        /* 0 means that dwm will calcul
 static const char *fonts[]          = { "MesloLGS NF:size=8" };
 static const char dmenufont[]       = "MesloLGS NF:size=8";
 
+/* static const char col_gray1[]       = "#222222";    
+static const char col_gray2[]       = "#444444";    
+static const char col_gray3[]       = "#bbbbbb";    
+static const char col_gray4[]       = "#eeeeee";    
+static const char col_cyan[]        = "#005577";    
+static const char *colors[][3]      = {    */
+        /*               fg         bg         border   */    
+/*        [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },    
+        [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },    
+};*/
+
 #include "/home/fuzzbox/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
@@ -80,6 +91,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-h", "32", "-l", "5", "-z", "800", "-p", "Run", NULL };
 static const char *dmenuwdcmd[] = { "dmenu-show-opened-windows", NULL };
 static const char *dmenucpcmd[] = { "clipmenu", "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-h", "32", "-l", "5", "-p", "Copy", NULL };
--- .dwm/dwm/config.h
+++ .dwm/dwm/config.h
@@ -4,21 +4,21 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int user_bh            = 32;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "MesloLGS NF:size=10" };
-static const char dmenufont[]       = "MesloLGS NF:size=10";
+static const char *fonts[]          = { "MesloLGS NF:size=8" };
+static const char dmenufont[]       = "MesloLGS NF:size=8";
 
 #include "/home/fuzzbox/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
 
 static const Rule rules[] = {
@@ -31,13 +31,14 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "URxvt",   NULL,     NULL,	       0,	  0,	      1,	   0,	     -1 },
+	{ "Thunar",  NULL,     NULL,	       0,	  1,	      0,	   0	     -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     	 = 0.5;  /* factor of master area size [0.05..0.95] */
+static const int nmaster     	 = 1;    /* number of clients in master area */
+static const int resizehints 	 = 0;    /* 1 means respect size hints in tiled resizals */
 static const int attachdirection = 0;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 
@@ -79,9 +80,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-l", "5", "-z", "800", "-p", "Run", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-h", "32", "-l", "5", "-z", "800", "-p", "Run", NULL };
 static const char *dmenuwdcmd[] = { "dmenu-show-opened-windows", NULL };
-static const char *dmenucpcmd[] = { "clipmenu", "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-l", "5", "-p", "Copy", NULL };
+static const char *dmenucpcmd[] = { "clipmenu", "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-c", "-bw", "1", "-h", "32", "-l", "5", "-p", "Copy", NULL };
 static const char *dmenustcmd[] = { "dmenu-stop", NULL };
 static const char *termcmd[]    = { "urxvt", NULL };
 
@@ -95,6 +96,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,        {0} },
 	{ MODKEY,                       XK_k, 	   focusstack,       {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,       {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_k,      pushdown,         {0} },
+	{ MODKEY|ShiftMask,             XK_j,      pushup,           {0} },
 	{ MODKEY,                       XK_i,      incnmaster,       {.i = +1 } },
 	{ MODKEY,                       XK_o,      incnmaster,       {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,         {.f = -0.05} },
@@ -124,11 +127,12 @@ static Key keys[] = {
 	{ MODKEY,			XK_g,	   setlayout,	     {.v = &layouts[11]} },
 	{ MODKEY,                       XK_f,      setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,        {.v = &layouts[2]} },
+	{ MODKEY,                       XK_r,      setlayout,        {.v = &layouts[7]} },
 	{ MODKEY,                       XK_space,  setlayout,        {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating,   {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,    {0} },
-	{ MODKEY,                       XK_0,      view,             {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,              {.ui = ~0 } },
+	{ MODKEY,                       XK_agrave, view,             {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_agrave, tag,              {.ui = ~0 } },
 	{ MODKEY,                       XK_Left,   focusmon,         {.i = -1 } },
 	{ MODKEY,                       XK_Right,  focusmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_Left,   tagmon,           {.i = -1 } },
@@ -153,6 +157,8 @@ static Key keys[] = {
 	{ 0, XK_Caps_Lock,		   	   spawn,	     SHCMD("kill -40 $(pidof dwmblocks)") },
 	{ 0, XK_Print,		   		   spawn,	     SHCMD("scrot -e 'mv $f ~/Images/Screenshots' && sleep 2 && notify-send Screenshot!!!") },
 	{ MODKEY|ShiftMask,		XK_b,	   spawn,	     SHCMD("firefox") },			
+	{ MODKEY|ShiftMask,		XK_c,	   spawn,	     SHCMD("urxvt -e nvim ~/.dwm/dwm/config.def.h") },
+	{ MODKEY|ShiftMask,		XK_r,	   spawn,	     SHCMD("urxvt -e ranger") },
 	{ MODKEY|ShiftMask,		XK_n,	   spawn,	     SHCMD("start-ncmpcpp") },			
 };
 
