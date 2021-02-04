4f82050ccd9cd28234fe6b8bd55b771ad253a6e2
Fuzzbox999 <fuzzbox.flc@gmail.com>
2021-02-04 12:21:30 +0100

    Don't start mopidy on startup, only on demand

--- .suckless/autostart.sh
+++ .suckless/autostart.sh
@@ -15,12 +15,6 @@ else
 	true
 fi
 
-if [ -z $(pgrep dwm-mpd) ]; then
-	dwm-mpd &
-else
-	true
-fi
-
 if [ -z $(pgrep udiskie) ]; then
 	udiskie --no-tray &
 else
@@ -28,7 +22,8 @@ else
 fi
 
 if [ -z $(pgrep picom) ]; then
-	picom --vsync --blur-background --active-opacity 1.0 --backend glx --opacity-rule "95:class_g = 'URxvt'" --opacity-rule "85:class_g = 'dmenu'" --opacity-rule "95:class_g = 'dwm'" -c -e 1.0 -i 0.9 -o 1.0 -D 6 --xinerama-shadow-crop -C &
+	picom --vsync --blur-background --active-opacity 1.0 --backend glx --opacity-rule "95:class_g = 'URxvt'" --opacity-rule "95:class_g = 'st-256color'" --opacity-rule "85:class_g = 'dmenu'" --opacity-rule "95:class_g = 'dwm'" -c -e 1.0 -i 0.9 -o 1.0 -D 6 --xinerama-shadow-crop -C &
+	# picom --vsync --blur-background --active-opacity 1.0 --backend glx -c -e 1.0 -i 1.0 -o 1.0 -D 6 --xinerama-shadow-crop -C &
 else
 	true
 fi
